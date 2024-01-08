d3a486f547caf63b1532d0e208fa1d27eea8f0c3
Shoaib <shoaibs411@gmail.com>
2024-01-09 01:28:08 +0530

    Provisioning RDS for Mysql DataBase

--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ module "docdb"{
   ENV                 = var.ENV
   DOCDB_PORT          = var.DOCDB_PORT
 }
+
+module "mysql"{
+  source              = "./vendor/modules/mysql"
+
+  ENV                 = var.ENV
+  DOCDB_PORT          = var.MYSQL_PORT
+}
