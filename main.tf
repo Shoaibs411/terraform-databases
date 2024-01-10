20757cfcada2832c8f2af0eab7c1a411f13708cd
Shoaib <shoaibs411@gmail.com>
2024-01-11 00:45:35 +0530

    Provisioning DB's - Creating RabbitMQ Server

--- main.tf
+++ main.tf
@@ -1,34 +1,43 @@
 module "docdb"{
-  source                = "./vendor/modules/docdb"
+  source                  = "./vendor/modules/docdb"
 
-  ENV                   = var.ENV
-  DOCDB_PORT            = var.DOCDB_PORT
-  DOCDB_INSTANCE_TYPE   = var.DOCDB_INSTANCE_TYPE
-  DOCDB_INSTANCE_COUNT  = var.DOCDB_INSTANCE_COUNT
-  DOCDB_ENGINE          = var.DOCDB_ENGINE
+  ENV                     = var.ENV
+  DOCDB_PORT              = var.DOCDB_PORT
+  DOCDB_INSTANCE_TYPE     = var.DOCDB_INSTANCE_TYPE
+  DOCDB_INSTANCE_COUNT    = var.DOCDB_INSTANCE_COUNT
+  DOCDB_ENGINE            = var.DOCDB_ENGINE
 }
 
 module "mysql"{
-  source                = "./vendor/modules/mysql"
+  source                  = "./vendor/modules/mysql"
 
-  ENV                   = var.ENV
-  MYSQL_PORT            = var.MYSQL_PORT
-  MYSQL_STORAGE         = var.MYSQL_STORAGE
-  MYSQL_ENGINE          = var.MYSQL_ENGINE
-  MYSQL_ENGINE_VERSION  = var.MYSQL_ENGINE_VERSION
-  MYSQL_ENGINE_FAMILY   = var.MYSQL_ENGINE_FAMILY
-  MYSQL_INSTANCE_CLASS  = var.MYSQL_INSTANCE_CLASS
+  ENV                     = var.ENV
+  MYSQL_PORT              = var.MYSQL_PORT
+  MYSQL_STORAGE           = var.MYSQL_STORAGE
+  MYSQL_ENGINE            = var.MYSQL_ENGINE
+  MYSQL_ENGINE_VERSION    = var.MYSQL_ENGINE_VERSION
+  MYSQL_ENGINE_FAMILY     = var.MYSQL_ENGINE_FAMILY
+  MYSQL_INSTANCE_CLASS    = var.MYSQL_INSTANCE_CLASS
 }
 
 module "redis"{
-  source                = "./vendor/modules/elasticcache"
+  source                  = "./vendor/modules/elasticcache"
 
-  ENV                   = var.ENV
-  REDIS_PORT            = var.REDIS_PORT
-  REDIS_ENGINE          = var.REDIS_ENGINE
-  REDIS_ENGINE_FAMILY   = var.REDIS_ENGINE_FAMILY
-  REDIS_ENGINE_VERSION  = var.REDIS_ENGINE_VERSION
-  REDIS_INSTANCE_COUNT  = var.REDIS_INSTANCE_COUNT
+  ENV                     = var.ENV
+  REDIS_PORT              = var.REDIS_PORT
+  REDIS_ENGINE            = var.REDIS_ENGINE
+  REDIS_ENGINE_FAMILY     = var.REDIS_ENGINE_FAMILY
+  REDIS_ENGINE_VERSION    = var.REDIS_ENGINE_VERSION
+  REDIS_INSTANCE_COUNT    = var.REDIS_INSTANCE_COUNT
+}
+
+module "rabbitmq" {
+  source                  = "./vendor/modules//rabbitmq"
+
+  ENV                     = var.ENV
+  RABBITMQ_PORT_NUMBER    = VAR.RABBITMQ_PORT_NUMBER
+  RABBITMQ_INSTANCE_TYPE  = VAR.RABBITMQ_INSTANCE_TYPE
+  
 }
 
 # We cannot parametrize anything that's added n the source.
